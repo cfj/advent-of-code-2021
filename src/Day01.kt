ce0d6cb911c35b5a5caeb41a9c7cf4d2cec565ef
Jonathan Svärdén <jonathan@accumbo.se>
2021-12-03 06:45:45 +0100

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map(String::toInt);
+        var previousDepth = 0;
+        var depthIncreases = 0;
+        for (depth in depths) {
+            if (depth > previousDepth) {
+                depthIncreases++;
+            }
+            previousDepth = depth;
+        }
+
+        return depthIncreases - 1;
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
